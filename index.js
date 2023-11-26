ddff4d4113e53a6c8089ad00f008a9aa12f53ee4
misha1290 <eremenko2006qwertyuiop@gmail.com>
2023-11-26 17:54:28 +0200

    1

---
+++ index.js
@@ -0,0 +1,37 @@
+let age = 17;
+console.log("Age:", age);
+
+let name = "Misha";
+console.log("Name:", name);
+
+let isStudent = true;
+console.log("Is Student:", isStudent);
+
+let myString = "Терпение и труд всё перетрут!";
+console.log("Favorite Quote:", myString);
+
+let myNumber = 1;
+myNumber += 10;
+console.log("Updated Number:", myNumber);
+
+let myNull = null;
+console.log("Null Variable:", myNull);
+
+let userName = prompt("Введіть ваше ім'я:");
+alert("Привіт, " + userName + "!");
+
+let userConfirmation = confirm("Підтвердіть вашу дію:");
+if (userConfirmation) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+
+alert("Увага! Дія небезпечна.");
+
+let userConfirmation2 = confirm("Підтвердіть вашу дію:");
+if (userConfirmation2) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
